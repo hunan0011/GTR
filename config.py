fda95b4851eab9c13b790e331d6c8ad14091a4d2
hunan0011 <1263474571@qq.com>
2026-05-22 15:08:05 +0800

    update config.py

--- config.py
+++ config.py
@@ -4,10 +4,8 @@ MEMMAP_PATH = "./data/msmarco/passage/msmarco-passages-bge.memmap"
 ID2OFFSET = "./data/msmarco/passage/msmarco-passages-bge_id2offset.tsv"
 EMBEDDING_DIM = 768
 
-PROJECT_ROOT = "."
 TREE_DIR = "./tree/cluster_docs_tree"
 
-OUTPUT_TREE_PATH = f"{TREE_DIR}/gmtree.pkl"
 CHILDREN_EMBEDDINGS_PATH = f"{TREE_DIR}/children_id_embeddings.pkl"
 ID2PATH = f"{TREE_DIR}/docs_id2path.pkl"
 LEAF2ID = f"{TREE_DIR}/leaf2docs.pkl"
@@ -20,25 +18,17 @@ TRAIN_QRELS = "./data/msmarco/passage/qrels.train.tsv"
 OUTPUT_DIR = "./output"
 
 MODEL_NAME = "./models/bge-base-en-v1.5"
-POOLING = "cls"                     # cls or mean
-MAX_SEQ_LEN = 512                   # you can change if needed
+POOLING = "cls"
+MAX_SEQ_LEN = 512
 
 LEARNING_RATE1 = 1e-5
 LEARNING_RATE2 = 5e-5
 WEIGHT_DECAY = 1e-3
-WARMUP_STEPS = 2000
 GRAD_CLIP = 1.0
 
 SAVE_INTERVAL = 1
 BATCH_SIZE = 128
-
-MAX_CANDIDATES = 100                # beam search pool
-ROUTING_BEAM_SIZE = 20
-
-RESUME = True
 EPOCHS = 15
-
-EVAL_TOPK = 100                     # retrieve top 100 documents for eval
 
 DEV_QUERYS = "./data/msmarco/passage/queries.dev.small.tsv"
 DEV_QRELS = "./data/msmarco/passage/qrels.dev.tsv"
@@ -54,5 +44,4 @@ NEG_NUM = 127
 PROB_THRESHOLD = 0.000077
 
 NODE_BALANCE = 13
-
 TREE_HEIGHT = 5
